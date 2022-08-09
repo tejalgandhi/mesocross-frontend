09ff9707fb5820e07031c7bda78c6a869bb6e838
Tejal Gandhi <tejal@twopeople.company>
2022-08-09 19:09:56 +0530

    Revert: payment method change1

--- components/Training/payment.vue
+++ components/Training/payment.vue
@@ -4,7 +4,6 @@
       {{ bodytitle }}
     </div>
     <input
-      v-if="isActive('alipay')"
       id="for10"
       name="paymethod"
       class="form-check-input"
@@ -12,11 +11,11 @@
       type="radio"
       @change="setSelectedCard(1)"
     >
-    <label v-if="isActive('alipay')" class="address-radio row mx-0 align-items-center mb-3" for="for10">
+    <label class="address-radio row mx-0 align-items-center mb-3" for="for10">
       <div class="col-12">
         <div class="form-check px-0">
           <div class="d-flex align-items-center">
-            <img src="~/assets/img/alipay.svg" class="mx-3" alt="image">
+            <img src="~/assets/img/Alipay.svg" class="mx-3" alt="image">
             <label class="ml-2 form-check-label font-16 text-dark" for="for10">
               <span class="d-block font-weight-bold">{{ $t('ali_pay') }}</span>
             </label>
@@ -25,7 +24,6 @@
       </div>
     </label>
     <input
-      v-if="isActive('wechat_pay')"
       id="for11"
       name="paymethod"
       class="form-check-input"
@@ -33,12 +31,12 @@
       type="radio"
       @change="setSelectedCard(2)"
     >
-    <label v-if="isActive('wechat_pay')" class="address-radio row mx-0 align-items-center mb-3" for="for11">
+    <label class="address-radio row mx-0 align-items-center mb-3" for="for11">
       <div class="col-12">
         <div class="form-check px-0">
           <div class="d-flex align-items-center">
             <div class="d-flex align-items-center">
-              <img src="~/assets/img/wechatpay.svg" class="mx-3" alt="image">
+              <img src="~/assets/img/Wechatpay.svg" class="mx-3" alt="image">
               <label class="ml-2 form-check-label font-16 text-dark" for="for11">
                 <span class="d-block font-weight-bold">{{ $t('wechat_pay') }}</span>
               </label>
@@ -48,38 +46,14 @@
       </div>
     </label>
     <input
-      v-if="isActive('multibanco')"
-      id="for12"
-      name="paymethod"
-      class="form-check-input"
-      :checked="selectedCard == 3"
-      type="radio"
-      @change="setSelectedCard(3)"
-    >
-    <label v-if="isActive('multibanco')" class="address-radio row mx-0 align-items-center mb-3" for="for12">
-      <div class="col-12">
-        <div class="form-check px-0">
-          <div class="d-flex align-items-center">
-            <div class="d-flex align-items-center">
-              <img src="~/assets/img/mb-icon.svg" class="mx-3" alt="image">
-              <label class="ml-2 form-check-label font-16 text-dark" for="for12">
-                <span class="d-block font-weight-bold">{{ $t('multibanco') }}</span>
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </label>
-    <input
-      v-if="isActive('bank_transfer')"
       id="for5"
       name="paymethod"
       class="form-check-input"
-      :checked="selectedCard == 4"
+      :checked="selectedCard == 0"
       type="radio"
-      @change="setSelectedCard(4)"
+      @change="setSelectedCard(0)"
     >
-    <label v-if="isActive('cash')" class="address-radio row mx-0 align-items-center mb-3" for="for5">
+    <label class="address-radio row mx-0 align-items-center mb-3" for="for5">
       <div class="col-12">
         <div class="form-check px-0 d-flex">
           <div class="d-flex align-items-center">
@@ -91,86 +65,49 @@
         </div>
       </div>
     </label>
-    <input
-      v-if="isActive('paypal')"
-      id="for13"
-      name="paymethod"
-      class="form-check-input"
-      :checked="selectedCard == 5"
-      type="radio"
-      @change="setSelectedCard(5)"
-    >
-    <label v-if="isActive('paypal')" class="address-radio row mx-0 align-items-center mb-3" for="for13">
-      <div class="col-12">
-        <div class="form-check px-0 d-flex">
-          <div class="d-flex align-items-center">
-            <img src="~/assets/img/paypal-icon.svg" class="mx-3" alt="image">
-            <label class="ml-2 form-check-label font-16 text-dark" for="for13">
-              <span class="d-block font-weight-bold">{{ $t('paypal') }}</span>
-            </label>
+    <div v-for="(card, index) in userCards" :key="index">
+      <input
+        :id="`for${index}`"
+        name="paymethod"
+        class="form-check-input"
+        type="radio"
+        :checked="selectedCard === card.id"
+        @change="setSelectedCard(card.id)"
+      >
+      <label class="address-radio row mx-0 align-items-center mb-3" :for="`for${index}`">
+        <div class="col-12">
+          <div class="form-check px-0 d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center">
+              <img :src="require(`~/assets/img/${setBrandImage(card.brand)}`)" class="mx-3" height="30px" width="50px" alt="image">
+              <label class="ml-2 form-check-label font-16 text-dark" :for="`for${index}`">
+                <span class="d-block font-weight-bold">{{ card.name }}</span>
+                <small>{{ card.number }}</small>
+              </label>
+            </div>
+            <p class="font-16 text-dark m-0 border-bottom d-inline text-right" @click="deleteCard(card.id)">{{ $t('checkout.delete_method') }}</p>
           </div>
         </div>
-      </div>
-    </label>
-    <div v-if="isActive('card')">
-      <div v-for="(card, index) in userCards" :key="index">
-        <input
-          :id="`for${index}`"
-          name="paymethod"
-          class="form-check-input"
-          type="radio"
-          :checked="selectedCard === card.id"
-          @change="setSelectedCard(card.id)"
-        >
-        <label class="address-radio row mx-0 align-items-center mb-3" :for="`for${index}`">
-          <div class="col-12">
-            <div class="form-check px-0 d-flex align-items-center justify-content-between">
-              <div class="d-flex align-items-center">
-                <img
-                  :src="require(`~/assets/img/${setBrandImage(card.brand)}`)"
-                  class="mx-3"
-                  height="30px"
-                  width="50px"
-                  alt="image"
-                >
-                <label class="ml-2 form-check-label font-16 text-dark" :for="`for${index}`">
-                  <span class="d-block font-weight-bold">{{ card.name }}</span>
-                  <small>**** **** **** {{ card.last4 }}</small>
-                </label>
-              </div>
-              <p class="font-16 text-dark m-0 border-bottom d-inline text-right" @click="deleteCard(card.id)">{{
-                $t('checkout.delete_method')
-                }}</p>
-            </div>
-          </div>
-        </label>
-      </div>
-      <div v-if="userCards.length > 0" class="col-md-4 py-4 mx-auto">
-        <button class="w-100 d-block btn btn-outline-dark" @click="setIsAddPayment(true)">
-          {{ $t('checkout.add_new_method') }}
-        </button>
-      </div>
-      <div v-else>
-        <label class="address-radio row mx-0 align-items-center mb-3 without-card">
-          <div class="addcard">
-            <input
-              id="forAddcart"
-              name="paymethod"
-              class="form-check-input"
-              type="radio"
-              @change="setSelectedCard('add_card')"
-            >
-            <label class="ml-2 form-check-label font-16 text-dark" for="forAddcart">
-              <img src="@/assets/img/card_mastercard.svg" alt="image">
-              <img src="@/assets/img/card_visa.svg" alt="image">
-              <img src="@/assets/img/card_american_express.svg" alt="image">
-            </label>
-            <button class="d-block btn btn-outline-dark" @click="setIsAddPayment(true)">
-              {{ $t('checkout.add_new_method') }}
-            </button>
-          </div>
-        </label>
-      </div>
+      </label>
+    </div>
+    <div v-if="userCards.length > 0" class="col-md-4 py-4 mx-auto">
+      <button class="w-100 d-block btn btn-light" @click="setIsAddPayment(true)">
+        {{ $t('checkout.add_new_method') }}
+      </button>
+    </div>
+    <div v-else>
+      <label class="address-radio row mx-0 align-items-center mb-3 without-card">
+        <div class="addcard">
+          <input id="forAddcart" name="paymethod" class="form-check-input" type="radio" @change="setSelectedCard('add_card')">
+          <label class="ml-2 form-check-label font-16 text-dark" for="forAddcart">
+            <img src="@/assets/img/card_mastercard.svg" alt="image">
+            <img src="@/assets/img/card_visa.svg" alt="image">
+            <img src="@/assets/img/card_american_express.svg" alt="image">
+          </label>
+          <button class="d-block btn btn-light" @click="setIsAddPayment(true)">
+            {{ $t('checkout.add_new_method') }}
+          </button>
+        </div>
+      </label>
     </div>
   </div>
 </template>
@@ -181,12 +118,6 @@
       bodytitle: {
         type: String,
         default: ''
-      }
-    },
-    data () {
-      return {
-        activePaymentMethods: [],
-        frontPayment: null
       }
     },
     computed: {
@@ -211,29 +142,18 @@
       }
 
     },
-    async mounted () {
-      this.frontPayment = await this.$store.dispatch('payment/payment')
-      this.fetchCards()
-      this.frontPayment.enabledPaymentMethods('subscription')
-        .then(({ data }) => {
-          console.log('Activated Payment methods = ', data)
-          this.activePaymentMethods = data
-        })
-        .catch(console.error)
+    mounted () {
+      this.getUserCards()
     },
     methods: {
-      isActive (m) {
-        return this.activePaymentMethods.includes(m)
-      },
-      fetchCards () {
-        this.frontPayment.cardList().then(({ data }) => {
-          console.log(data)
-          this.getUserCards({ data })
-        })
-      },
       async deleteCard (card) {
-        await this.frontPayment.deleteCard(card)
-        this.fetchCards()
+        const { data } = await this.$axios.post('/stripe/delete-card', { card_id: card })
+        this.$toast.success(data.message, { duration: 5000, position: 'top-right', className: 'custom-toast-success-class' })
+        if (card === this.selectedCard) {
+          this.setSelectedCard(0)
+        }
+
+        this.getUserCards()
       },
       ...mapMutations({
         setIsAddPayment: 'user/setIsAddPayment',

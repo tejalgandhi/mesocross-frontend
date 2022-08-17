caafcc48180b9c03da430d7b92c5c666df685dfd
Tejal Gandhi <tejal@twopeople.company>
2022-08-17 17:06:45 +0530

    comment user section

--- components/theme/Header/UserSection/Index.vue
+++ components/theme/Header/UserSection/Index.vue
@@ -21,15 +21,15 @@
       <img src="@/assets/img/search.svg" alt="search">
     </figure>
     <figure v-click-outside="hideUserTab" class="mb-0 text-uppercase">
-      <caption v-if="!showUserTab" :class="{'active':showUserTab}" @click="showUserTab = !showUserTab">
+      <div v-if="!$auth.loggedIn" :class="{'active':showUserTab}" @click="showUserTab = !showUserTab">
         Sign in
-      </caption>
-      <caption v-else :class="{'active':showUserTab}" @click="showUserTab = !showUserTab">
+      </div>
+      <div v-else :class="{'active':showUserTab}" @click="showUserTab = !showUserTab">
         <img src="@/assets/img/user.svg" alt="search" class="mr-3">
-      </caption>
-<!--      <transition name="pop" v-if="!showUserTab">-->
-<!--        <span  class="info" />-->
-<!--      </transition>-->
+      </div>
+      <transition name="pop" v-if="!$auth.loggedIn">
+        <span  class="info" />
+      </transition>
       <transition name="show">
         <user-tab v-if="showUserTab" @close="showUserTab = false" />
       </transition>

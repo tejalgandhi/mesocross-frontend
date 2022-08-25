8c4203b9dcaff3d9654f03507e356fbc3d72da82
Tejal Gandhi <tejal@twopeople.company>
2022-08-25 11:28:26 +0530

    filter changes

--- pages/products.vue
+++ pages/products.vue
@@ -20,7 +20,7 @@
               <div class="col col order-2 order-sm-0">
                 <div v-show="selectedFilters && selectedFilters.length >0" class="selected_filter">
                   <a v-for="(filter, index) in selectedFilters" :key="index" class="mr-2 mb-2" href="javascript:void(0)" @click="removeFilter(index)">
-                    {{ filter.name }} <img src="@/assets/img/filter-cross.svg" alt="image">
+                    {{ filter.name }} 11<img src="@/assets/img/filter-cross.svg" alt="image">
                   </a>
                 </div>
               </div>
@@ -120,7 +120,6 @@ export default {
   auth: false,
   async asyncData ({ params, $axios, store }) {
     let url = '/products?page=1'
-    console.log(params)
     if (params && params.treatmentSlug) {
       url = `${url}&treatment_solutions=${params.treatmentSlug}`
     } else if (params && params.categorySlug) {
@@ -272,7 +271,15 @@ export default {
           }
         })
         if (activeCat !== undefined) {
-          activeCat = `&category=${activeCat.id}`
+          if (activeCat.parent_id === null && activeCat.child.length > 0 && activeCat.is_skincare === false) {
+            const selectedCat = [...new Map(activeCat.child.map(item =>
+              [item.id, item.id])).values()]
+            selectedCat.push(activeCat.id)
+            const selectedString = selectedCat.toString()
+            activeCat = `&category=${selectedString}`
+          } else {
+            activeCat = `&category=${activeCat.id}`
+          }
         }
       }
       let url = productUrl
@@ -281,8 +288,8 @@ export default {
         if (this.selectedFilters) {
           const category = this.selectedFilters.filter(v => !v.treatmentSolution)
           if (category.length > 0) {
-            const cats = category.filter(c => !c.is_skincare).map(f => f.id).toString()
             const skins = category.filter(c => c.is_skincare).map(f => f.id).toString()
+            const cats = category.filter(c => !c.is_skincare).map(f => f.id).toString()
             if (cats && skins) {
               url = `${url}&category=${cats}&skincare=${skins}`
             } else if (cats) {

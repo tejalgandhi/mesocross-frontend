bba00412539d9e98bdae14b5afeec7003da51ecc
Tejal <tejal@enstead.com>
2023-07-05 14:48:42 +0530

    load lang

--- components/Ui/Dashboard/Orders/Index.vue
+++ components/Ui/Dashboard/Orders/Index.vue
@@ -148,7 +148,6 @@ export default {
 
     handleData (data) {
       this.entries = data.map((el) => {
-        console.log(el.payment_method)
         console.log(this.$t(`order.method.${el.payment_method}`))
         const entries = [
           {

6d72ae42994a14d601d2398029f4f9c08f866de8
Pedro Teixeira <75336401+PedroTeixeira-unP@users.noreply.github.com>
2023-08-02 12:21:07 +0100

    update forgot password

--- pages/forgot-password.vue
+++ pages/forgot-password.vue
@@ -61,15 +61,11 @@ export default {
               this.successRes = true
             })
             .catch((error) => {
-              if (error.response.status === 404) {
-                this.errorRes = true
-                this.errorMsg = 'User doesn\'t exist with given email. '
-              } else if (error.response.status === 429) {
+              if (error.response.status === 429) {
                 this.errorRes = true
                 this.errorMsg = 'Too many attampt please try after sometime. '
               } else if (error.response.status === 500) {
-                this.errorRes = true
-                this.errorMsg = error.response.data.message
+                this.successRes = true
               }
             })
         }
